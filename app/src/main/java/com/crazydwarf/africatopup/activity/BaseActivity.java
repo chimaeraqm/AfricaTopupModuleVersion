d0427101cc3ac38a2107a44402d989a9f38e5bbd
chimaeraqm <chimaeraqm@126.com>
2018-10-25 00:42:28 +0800

    debug version 0.1.20181025.1
    -replace app icon
    
    -fix toolbar display to fit layout better

--- app/src/main/java/com/crazydwarf/africatopup/activity/BaseActivity.java
+++ app/src/main/java/com/crazydwarf/africatopup/activity/BaseActivity.java
@@ -1,9 +1,13 @@
 package com.crazydwarf.africatopup.activity;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.crazydwarf.africatopup.Utilities.ActivityManager;
 import com.crazydwarf.africatopup.Utilities.AppLanguageUtils;
@@ -14,6 +18,13 @@ public class BaseActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityManager.getInstance().addActivity(this);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            Window window = getWindow();
+//            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//            window.setFlags(
+//                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+//                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
     }
 
     @Override
